427e7f1e8b55353fb6d595effa44082d803e1494
TheLsbt <tyresewolmarans@gmail.com>
2024-06-15 00:41:21 +0200

    Update WebhookExample.gd

--- examples/basic_example/WebhookExample.gd
+++ examples/basic_example/WebhookExample.gd
@@ -1,14 +1,12 @@
 extends Node
 
-const WEBHOOK_URL: String = "https://discord.com/api/webhooks/1250442585722589195/iETRcOV7GCWwV1wVYTPs0ZgtelG6DJB6_uF0foQH0UEGUJB9FQOCnZPz9e_nPjbdpfr3"
+const WEBHOOK_URL: String = ""
 
 var webhook: DiscordWebHook = null
 
 
 func _ready() -> void:
 	webhook = DiscordWebHook.new(WEBHOOK_URL)
-	webhook.add_poll(
-		"Question", ["Awnser 1", "Awnser 2", "Awnser 3"], 1, true
-	)
-	var response = await webhook.post()
-	print(response.text())
+	webhook.message("Hello from godot!")
+	webhook.post()
+
